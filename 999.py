32e64bb6ee7a55a2634e15410fca1d675bcc8084
Manuela Ramos <128739474+manuelaramos200@users.noreply.github.com>
2023-07-26 15:16:42 -0300

    Create 999.py

---
+++ 999.py
@@ -0,0 +1,9 @@
+n = soma = cont = 0
+while n != 999:
+    n = int(input('digite um numero: [999 para parar] '))
+    soma += n 
+    cont += 1
+print(cont - 1 ,soma - 999)
+
+---------------------------------------------
+
