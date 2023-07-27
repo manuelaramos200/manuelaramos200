18fa398756a2444dc00f7974f762305bbe36b702
Manuela Ramos <128739474+manuelaramos200@users.noreply.github.com>
2023-07-27 14:10:10 -0300

    Update vetor.py

--- vetor.py
+++ vetor.py
@@ -1,3 +1,22 @@
+resp = 'S'
+n = quant = media = soma = menor = maior = 0
+while resp in 'Ss':
+    n = int(input('digite um numero:'))
+    soma += n
+    quant += 1
+    media =  soma / quant
+    if quant == 1:
+        maior = menor = n
+    else: 
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
+    resp = str(input('deseja continuar [S/N]:')).upper().strip()[0]
+print('voce digitou {} numero e media foi {}'.format(quant,media))
+print( 'o maior valor é {} o menor valor é {}'.format(maior,menor))
+print('acabou')
+------------------------------------------------------------------
 n = cont = soma = media = menor = maior = 0
 c = 'S/N'
 while c not in 'Nn':
