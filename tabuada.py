86250e92acc214aade78fa0c36b3ed1d3807bdf4
Manuela Ramos <128739474+manuelaramos200@users.noreply.github.com>
2023-08-01 11:32:06 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -1,1 +1,8 @@
+tabuada = cont = 1
 
+while True:
+    tabuada = int(input('qual tabuada você deseja ver?'))
+    if tabuada >= 1:
+        if cont < 10:
+            cont *= tabuada
+            print(tabuada,cont)
