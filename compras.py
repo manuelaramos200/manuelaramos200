f9aebdee9ae29cc02a4ad880197dda6b946b0c18
Manuela Ramos <128739474+manuelaramos200@users.noreply.github.com>
2023-08-04 15:39:03 -0300

    Update compras.py

--- compras.py
+++ compras.py
@@ -1,20 +1,20 @@
-totcompra = maiormil = menor = 0
+totcompra = maiormil = menor = cont = 0
+barato = ''
 while True:
     nome = str(input('Qual o nome do produto: '))
     preco = int(input('Qual o preço do produto: '))
     totcompra += preco
-    conti = ' '
+    cont +=1
     if preco > 1000:
         maiormil += 1
-    if preco == 1:
-        menor = nome
-    else:
-        preco == menor
-        menor = nome
+    if cont == 1 or preco < menor :
+        menor = preco
+        barato = nome
+    conti = ' '
     while conti not in 'SN':
         conti = str(input('Deseja continuar? [S/N]:')).strip().upper()[0]
     if conti == 'N':
         break
 print(f'O total da compra foi R${totcompra}')
 print(f'{maiormil} Produtos custaram mais de R$1000 ')
-print(f' o {menor} que foi mais barato')
+print(f' o {barato} mais barato custour {menor}')
