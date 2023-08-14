63b14db76fdd9a04b81240f18936f496b0a0afd4
Manuela Ramos <128739474+manuelaramos200@users.noreply.github.com>
2023-08-14 16:02:40 -0300

    Create numlista.py

---
+++ numlista.py
@@ -0,0 +1,8 @@
+from random import randint
+numero = randint(1,10),randint(1,10),randint(1,10),randint(1,10),randint(1,10)
+print(f'eu sorteei{numero}')
+for n in numero:
+    print(f'{n}')
+print(f'o maior valor é {max(numero)}')
+print(f'o menor valor é {min(numero)}')
+    
