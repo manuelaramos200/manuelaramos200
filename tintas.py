42fdb1babdaa9080a0f6fc22c04dde7e0bd52c7a
Manuela Ramos <128739474+manuelaramos200@users.noreply.github.com>
2024-08-12 09:02:02 -0300

    Update tintas.py

--- tintas.py
+++ tintas.py
@@ -6,7 +6,10 @@ def calcTintas(area):
     latas = math.ceil(litros/18)
     custo = latas * 80
     print('-' * 40)
-    print(f'Para pintar essa área, Você precisará de {litros:.0f} Litros e equivale a {latas:.0f} latas de 18L, valor a pagar = {custo} ')
+    print(f'Você precisará de {litros:.0f} Litros:\n{latas:.0f} latas de 18L,')
+    print(f'Total à vista em cartão = R${custo}\n'
+          f'Parcelado em 12X = R${(custo + (custo * 6.5 / 100))/12:.2f}\n'
+          f'valor via Pix = R${custo - (custo * 15/100)}')
     print('-' * 40)
 
 
@@ -15,7 +18,10 @@ def calcG(area):
     galoes = math.ceil(litros/3.6)
     custo = galoes * 25
     print('-' * 40)
-    print(f'Para pintar essa área, Você precisará de {litros:.0f} Litros e equivale a {galoes:.0f} galoes de 3,6L, valor a pagar = {custo} ')
+    print(f'Você precisará de {litros:.0f} Litros e equivale a {galoes:.0f} Galoes de 3,6L,')
+    print(f'Total à vista em cartão = R${custo}\n'
+          f'Parcelado em 12X = R${(custo + (custo * 3.6 / 100)) / 12:.2f}\n'
+          f'valor via Pix = R${custo - (custo * 15 / 100)}')
     print('-' * 40)
 
 
@@ -24,12 +30,21 @@ def calcP(area):
     potes = math.ceil(litros/2.4)
     custo = potes * 18
     print('-' * 40)
-    print(f'Para pintar essa área, Você precisará de {litros:.0f} Litros e equivale a {potes:.0f} latas de 2,4L, valor a pagar = {custo} ')
+    print(f'Você precisará de {litros:.0f} Litros e equivale a {potes:.0f} Potes de 2,4L, ')
+    print(f'Total à vista em cartão = R${custo}\n'
+          f'Parcelado em 12X = R${custo + (custo * 2.4 / 100)/12:.2f}'
+          f'valor via Pix = R${custo - (custo * 15 / 100)}')
     print('-' * 40)
+
+def calcMistura(area):
+
+    
+
 
 
 area = int(input('Área da parede: '))
 calcTintas(area)
 calcG(area)
 calcP(area)
+calcMistura(area)
 
